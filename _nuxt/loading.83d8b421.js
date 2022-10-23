7f4f10294e49d56a4e3d40f871743a89c17c5abb
NenoPr <nenadpredan@gmail.com>
2022-10-24 01:02:18 +0200

    8c9591b

---
+++ _nuxt/loading.83d8b421.js
@@ -0,0 +1,1 @@
+import{a,o as s,b as c,p as t,j as n,e}from"./entry.29b98181.js";const d={name:"LoadingComponent"},p=o=>(t("data-v-c59e24bc"),o=o(),n(),o),i=p(()=>e("div",{class:"loading"},[e("span"),e("div",{class:"loading-word"},"Loading...")],-1)),_=[i];function r(o,l,m,f,g,h){return s(),c("div",null,_)}const v=a(d,[["render",r],["__scopeId","data-v-c59e24bc"]]);export{v as l};
